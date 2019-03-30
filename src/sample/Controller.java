2ef1cc5f420ebcbababbce93c14c7ba37fd63c1e
Flow-flex-the-complex <fth29@hi.is>
2019-03-30 19:54:41 +0000

    Gjösso vel EMMA

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,7 +3,6 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.InputMethodEvent;
 
