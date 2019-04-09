812f867d88f22cc61a67438849002eb5803871e6
emmalif96 <43357822+emmalif96@users.noreply.github.com>
2019-04-09 22:38:27 +0000

    controller fyrir booking

---
+++ src/Controller/BookingController.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import Model.Booking;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class BookingController {
+
+
+        @FXML
+        private TextField CustomerFirstname;
+
+        @FXML
+        private TextField CustomerNumber;
+
+        @FXML
+        private TextField CustomerEmail;
+
+        @FXML
+        private TextField CustomerLastname;
+
+        @FXML
+        private Slider CustomerSeats;
+
+        public Booking booking;
+
+
+    }
+
+
