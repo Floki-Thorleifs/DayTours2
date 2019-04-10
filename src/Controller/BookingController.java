cd37fad21261f222555433c1cb69f88ac1141328
Egill Ragnarsson <egill@vr2t-dw079.rhi.hi.is>
2019-04-10 02:26:41 +0000

    booking

--- src/Controller/BookingController.java
+++ src/Controller/BookingController.java
@@ -4,13 +4,19 @@ import Model.Booking;
 import Model.Trip;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class BookingController {
 
@@ -31,9 +37,8 @@ public class BookingController {
 
         private Trip selectedTrip;
 
+        private Booking booking;
 
-
-        public Booking booking;
 
 
         public void initData(Trip trip) {
@@ -42,7 +47,42 @@ public class BookingController {
 
 
         public void bookHandler(ActionEvent actionEvent) {
-                
+                Boolean booked = addBooking();
+
+                if (booked) {
+
+                        int id = selectedTrip.getId();
+                        String firstName = CustomerFirstname.getText();
+                        String lastName = CustomerFirstname.getText();
+                        String email = CustomerFirstname.getText();
+                        String phonenumber = CustomerFirstname.getText();
+                        int seats = Integer.parseInt(CustomerFirstname.getText());
+                        String date = selectedTrip.getDate();
+
+                        booking = new Booking(id, firstName, lastName, email, phonenumber, seats, date);
+
+                        try {
+                                FXMLLoader loader = new FXMLLoader();
+                                loader.setLocation(getClass().getClassLoader().getResource("./View/TripView.fxml"));
+                                Parent tableViewParent = loader.load();
+
+                                Scene tableViewScene = new Scene(tableViewParent);
+
+                                TripController controller = loader.getController();
+
+//                                controller.initData(selectedTrip, booking);
+        
+                                Stage window = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+
+                                window.setScene(tableViewScene);
+                                window.show();
+                        } catch (IOException ex) {
+                                System.out.println("Json villa");
+                        }
+
+                } else {
+                        System.out.println("TODO gera villusidu");
+                }
         }
 
 //                DayTours trips = new DayTours();
@@ -67,6 +107,7 @@ public class BookingController {
                         newJsonObject.put("email", CustomerEmail.getText());
                         newJsonObject.put("phonenumber", CustomerNumber.getText());
                         newJsonObject.put("seats", Integer.parseInt(CustomerSeats.getText()));
+                        newJsonObject.put("date", selectedTrip.getDate());
 
                         jsonArray.add(newJsonObject);
 
@@ -78,6 +119,7 @@ public class BookingController {
 
                 } catch (Exception e) {
                         System.out.println("Failed.");
+                        return false;
                 }
                 return true;
         }
